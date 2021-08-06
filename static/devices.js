2b3a1f429170cec8f52fa7ff05f5971eb503c0ab
akulchhillar <32826048+akulchhillar@users.noreply.github.com>
2021-08-06 23:21:35 +0530

    Update devices.js

--- static/devices.js
+++ static/devices.js
@@ -1,5 +1,6 @@
-let socket = io.connect(window.origin +"/device")
-let room = window.location.pathname.split("/")[2]
+let socket = io.connect("http://127.0.0.1:5001/device")
+let room = "seahawk"
+//This is a hardcoded uid of the restaurant
 
 socket.on("connect",()=>{socket.send(room)})
 
