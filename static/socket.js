012b4273878f703ebd8871a5df87e2ce3073901a
akulchhillar <32826048+akulchhillar@users.noreply.github.com>
2021-08-06 23:44:53 +0530

    Update socket.js

--- static/socket.js
+++ static/socket.js
@@ -1,6 +1,8 @@
 let socket = io.connect(window.origin +"/restaurant")
 let room = window.location.pathname.split("/")[2]
 //visit seahawk as this is the hard-coded uid for the device
+// http://127.0.0.1:5001/restaurant/seahawk
+
 
 socket.on("connect",()=>{socket.send(room)})
 
